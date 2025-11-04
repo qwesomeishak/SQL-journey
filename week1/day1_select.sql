e62b9e7734970ef31f29b8e0ceeabcebacdf655e
qwesomeishak <143293900+qwesomeishak@users.noreply.github.com>
2025-11-04 14:04:32 +0100

    Day 1 SELECT queries completed

--- week1/day1_select.sql
+++ week1/day1_select.sql
@@ -1,4 +1,25 @@
--- Day 1: Basic SELECT Queries
--- Test query (you will replace this later)
+-- 1. Show all employees
 
-SELECT 'Hello SQL Learning!' AS message;
+SELECT * FROM Employee;
+
+-- 2. Show only first name, last name, and office_id
+
+SELECT Frist_name,Last_name,Office_id FROM Employee;
+
+-- 3. Show distinct marital statuses
+
+SELECT DISTINCT Marital_status FROM Employee;
+
+-- 4. List all employees hired after 2019
+
+SELECT Frist_name,Last_name,Date_hired
+FROM Employee
+WHERE Date_hired > '2019-12-31';
+
+-- 5. Show all female staff from the HR department
+
+SELECT E.Frist_name,E.Last_name, D.dept_name
+FROM Employee E
+JOIN Department D
+ON E.Dept_id=D.dept_id
+WHERE E.Gender= 'F' AND D.dept_name='Human Resources';
